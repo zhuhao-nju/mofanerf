e0c4d01ff25847b54039666c75a0f3c183bd985e
neverstopZyy <42933584+neverstopZyy@users.noreply.github.com>
2022-10-11 14:17:33 +0800

    Update google link

--- download_pretrained_models.sh
+++ download_pretrained_models.sh
@@ -1,3 +1,5 @@
+due 
+
 # check if samples.tar.gz exists
 if [ -f ./pretrained_models.tar.gz ] ; then
 echo "pretrained_models.tar.gz has already been downloaded."
@@ -5,8 +7,9 @@ fi
 
 # download samples.tar.gz
 if [ ! -f ./pretrained_models.tar.gz ] ; then
-wget --no-check-certificate 'https://box.nju.edu.cn/f/aad38f30d45d41a1a78f/?dl=1' -O ./pretrained_models.tar.gz
+wget --no-check-certificate 'https://drive.google.com/u/0/uc?id=1jlhtm8BoChczSMks3WjChQ_SGLXyZ9ka&export=download' -O ./pretrained_models.tar.gz
 fi
+# wget --no-check-certificate 'https://box.nju.edu.cn/f/aad38f30d45d41a1a78f/?dl=1' -O ./pretrained_models.tar.gz
 
 # extract files
 mkdir -p ./logs/
