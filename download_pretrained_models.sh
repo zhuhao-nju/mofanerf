11f9965acfa43ffa50eadc2c3000fd4bd69c96b4
zhuhao-nju <40872587+zhuhao-nju@users.noreply.github.com>
2023-06-04 03:14:50 +0800

    Update download_pretrained_models.sh

--- download_pretrained_models.sh
+++ download_pretrained_models.sh
@@ -9,7 +9,7 @@ fi
 if [ ! -f ./pretrained_models.tar.gz ] ; then
 wget --no-check-certificate 'https://drive.google.com/u/0/uc?id=1jlhtm8BoChczSMks3WjChQ_SGLXyZ9ka&export=download' -O ./pretrained_models.tar.gz
 fi
-# wget --no-check-certificate 'https://box.nju.edu.cn/f/aad38f30d45d41a1a78f/?dl=1' -O ./pretrained_models.tar.gz
+# wget --no-check-certificate 'https://box.nju.edu.cn/f/8e6a8048dfc14ef99c7a/?dl=1' -O ./pretrained_models.tar.gz
 
 # extract files
 mkdir -p ./logs/
